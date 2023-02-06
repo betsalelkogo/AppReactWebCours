f3f08e433e745cf70d291c12a7720936bde88ca0
betsalel koginsky <betsalelkogo@gmail.com>
2023-02-06 11:11:06 +0200

    fix user api client

--- api/UserApi.tsx
+++ api/UserApi.tsx
@@ -1,14 +1,14 @@
 import apiClient from "./ClientApi";
 
 const login = async (email: string, password: string) => {
-  return apiClient.get("/login", { email, password });
+  return apiClient.get("/auth/login", { email, password });
 };
 const register = async (name: string, email: string, password: string) => {
-  return apiClient.post("/register", { name, email, password });
+  return apiClient.post("/auth/register", { name, email, password });
 };
 
 const logout = async () => {
-  return apiClient.post("/logout");
+  return apiClient.post("/auth/logout");
 };
 
 const uploadImage = async (image: any) => {
